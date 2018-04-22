77a9aa8e89c56fd35eb44fabda4f456a709cead1
evanwsun <ews170030@utdallas.edu>
2018-04-21 22:27:40 -0500

    More app

--- app.py
+++ app.py
@@ -11,18 +11,13 @@ app = Flask(__name__)
 @app.route('/')
 def output():
 
-    # serve index template
     return render_template('index.html', url = "api/google")
 
 @app.route('/api/google')
 def random_number():
-    response = {
-        'randomNumber': randint(1, 100)
-    }
-    return jsonify(response)
+
 
 if __name__ == '__main__':
     # run!
     gps.__init__('practice.json')
-    print(gps.getBikeDistance())
     app.run()
