99933b4d4dd10065dce62baf91d94297454aba6e
evanwsun <ews170030@utdallas.edu>
2018-04-22 00:03:34 -0500

    Rip

--- app.py
+++ app.py
@@ -4,20 +4,43 @@ import numpy as np
 import base64
 import gps
 from random import *
-
+import fastFood
+from matplotlib.backends.backend_agg import FigureCanvasAgg as FigureCanvas
+from matplotlib.figure import Figure
+from matplotlib.dates import DateFormatter
+import matplotlib.pyplot as plt
+import datetime
+from io import StringIO, BytesIO
+import random
 
 app = Flask(__name__)
 
 @app.route('/')
 def output():
-
-    return render_template('index.html', url = "api/google")
+    return render_template('charts.html', url = "api/google", figure = "fig1.png")
 
 @app.route('/api/google')
 def random_number():
+    response = {
+        'randomNumber': randint(1, 100)
+    }
+    return jsonify(response)
 
+@app.route('/fig1.png')
+def figure():
+    figure = plt.figure()
+    figure.axes.append(fastFood1.printSumGraph())
+    figure.autofmt_xdate()
+    canvas = FigureCanvas(figure)
+    png_output = BytesIO()
+    canvas.print_png(png_output)
+    response = make_response(png_output.getvalue())
+    response.headers['Content-Type'] = 'image/png'
+    return response
 
 if __name__ == '__main__':
     # run!
+    fastFood1 =fastFood.FastFood('transactions(1).csv')
+
     gps.__init__('practice.json')
     app.run()
---
+++ fastFood.py
@@ -0,0 +1,77 @@
+import pandas as pd
+import matplotlib.pyplot as plt
+
+
+class FastFood:
+    def getCount(self, m,y):
+        self.currentYear = self.xf[self.xf['Year'] == str(y)]
+        self.currentMonth = self.currentYear[self.currentYear['Month'] == str(m) ]
+        return(len(self.currentMonth.index))
+
+    def getSum(self, m,y):
+        currentYear = self.xf[self.xf['Year'] == str(y)]
+        currentMonth = currentYear[currentYear['Month'] == str(m)]
+        self.xf['Sum'] = len(currentMonth.index)
+        return(pd.to_numeric(currentMonth['Amount']).sum())
+
+    def __init__(self, file):
+        self.x = pd.read_csv(file)
+        self.x = self.x.fillna(' ')
+        #print(x)
+
+        self.xf = self.x[self.x['Category'] == 'Fast Food']
+        self.xf['Month'] = self.xf.Date.str[:2]
+        self.xf['Year'] = self.xf.Date.str[-2:]
+        month = ['January', 'February', 'March', 'April', 'May', 'June', 'July', 'August', 'September', 'October',
+                 'November', 'December', 'January', 'February', 'March', 'April', 'May', 'June', 'July', 'August',
+                 'September', 'October', 'November', 'December']
+        year = ['17', '17', '17', '17', '17', '17', '17', '17', '17', '17', '17', '17', '18', '18', '18', '18', '18',
+                '18', '18', '18', '18', '18', '18', '18']
+        self.graph = pd.DataFrame(columns=['Month', 'Year', 'Count', 'Sum'])
+        self.graph['Month'] = month
+        self.graph['Year'] = year
+        self.graph['Count'] = 0
+        self.graph['Sum'] = 0.0
+
+        for y in range(17, 19):
+            for m in range(1, 13):
+                if m < 10:
+                    month = '0' + str(m)
+                else:
+                    month = str(m)
+                if (y == 17):
+                    self.graph.at[int(m) - 1, 'Count'] = self.getCount(month, y)
+                    self.graph.at[int(m) - 1, 'Sum'] = self.getSum(month, y)
+                if (y == 18):
+                    self.graph.at[int(m) + 11, 'Count'] = self.getCount(month, y)
+                    self.graph.at[int(m) + 11, 'Sum'] = self.getSum(month, y)
+
+
+
+
+
+
+
+            #print('Month = '+str(m)+'   Year = '+str(y))
+            #print('Count = '+ str(getCount(m,y)))
+            #print('Sum = $'+ str(getSum(m,y))+ '\n')
+
+
+    def printCountGraph(self):
+        self.graph.index= self.graph.Month+' '+self.graph.Year
+        ax = self.graph[['Month','Count']].plot(kind='bar', color='c', title ="Frequency of Fast Food Consumption", figsize=(10, 5), legend=True, fontsize=12)
+        ax.set_xlabel("Month", fontsize=8)
+        ax.set_ylabel("# Times per Month", fontsize=12)
+        return ax
+
+    def printSumGraph(self):
+        self.graph.index = self.graph.Month + ' ' + self.graph.Year
+        ax = self.graph[['Month', 'Sum']].plot(kind='bar', color='c',title="Amount Spent on Fast Food", figsize=(10, 5), legend=True, fontsize=12)
+        ax.set_xlabel("Month", fontsize=8)
+        ax.set_ylabel("Amount Spent ($)", fontsize=12)
+        return ax
+
+
+if __name__ == '__main__':
+    fastFoodObj = FastFood('transactions.csv')
+
