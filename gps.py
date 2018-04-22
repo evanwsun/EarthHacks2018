06c3e0517cc1307dde5f8240510d82565ca0090a
Alex <alexpolednik@gmail.com>
2018-04-21 22:57:43 -0500

    old

--- gps.py
+++ gps.py
@@ -1,10 +1,7 @@
 import json
 from math import sin,cos,atan2,sqrt,radians
 import pandas as pd
-<<<<<<< HEAD
-=======
 import os
->>>>>>> 927d6ebc18f1e152d1ea65e71956952186b5aea1
 
 walkDist = 0
 driveDist = 0
@@ -15,48 +12,6 @@ times = []
 distance = 0
 xDiff = 0
 yDiff = 0
-<<<<<<< HEAD
-with open("history.json") as data:
-    d = json.load(data)
-    jData = d["locations"]
-    for location in jData:
-        xCor.append(location.get("latitudeE7") / 1E7)
-        yCor.append(location.get("longitudeE7") / 1E7)
-        times.append(location.get("timestampMs"))
-    df = pd.DataFrame({'Longitude': xCor, 'Latitude': yCor, 'Time': times})
-    print(df)
-
-for i in range(1,len(xCor)):
-    curXCor = radians(xCor[i])
-    prevXCor = radians(xCor[i-1])
-    curYCor = radians(yCor[i])
-    prevYCor = radians(yCor[i-1])
-    yDiff = abs(curXCor-prevXCor)
-    xDiff = abs(curYCor - prevYCor)
-    curTime = int(times[i])
-    prevTime = int(times[i-1])
-    # print("cur:",curTime)
-    # print("prev",prevTime)
-    tDiff = abs(curTime-prevTime)/60.0/60.0/1000
-    # print("time:", tDiff)
-    a = (sin(yDiff / 2))**2 + cos(prevYCor)*cos(curYCor)*(sin(xDiff/2))**2
-    if ((1-a) > 0 and 1-a < 1):
-        c = 2*atan2(a**0.5, (1 - a)**0.5)
-        dist = 3961 * c
-        # print("Dist:",dist)
-        speed = dist/tDiff
-        # print("Speed:", speed)
-        if (speed < 5):
-            walkDist += dist
-        elif(speed <15):
-            bikeDist += dist
-        else:
-            driveDist += dist
-
-print("walk:",walkDist)
-print("bike:", bikeDist)
-print("drive:",driveDist)
-=======
 global filename
 fileName = "practice.json"
 
@@ -90,7 +45,7 @@ def getData():
         prevTime = int(times[i-1])
         tDiff = abs(curTime-prevTime)/60.0/60.0/1000
         a = (sin(yDiff / 2))**2 + cos(prevYCor)*cos(curYCor)*(sin(xDiff/2))**2
-        if ((1-a) > 0 and (1-a) < 1):
+        if ((1-a) > 0 and 1-a < 1):
             c = 2*atan2(a**0.5, (1 - a)**0.5)
             dist = 3961 * c
             speed = dist/tDiff
@@ -135,4 +90,3 @@ def __init__(filename):
     getData()
 
 getData()
->>>>>>> 927d6ebc18f1e152d1ea65e71956952186b5aea1
