4c89c0ab57285ecfc80d95e897d83739f38a1439
Alex <alexpolednik@gmail.com>
2018-04-21 21:29:32 -0500

    Module Attempt

--- gps.py
+++ gps.py
@@ -2,8 +2,11 @@ import json
 from math import sin,cos,atan2,sqrt,radians
 import pandas as pd
 
+global walkDist
 walkDist = 0
+global driveDist
 driveDist = 0
+global bikeDist
 bikeDist = 0
 xCor = []
 yCor = []
@@ -11,43 +14,54 @@ times = []
 distance = 0
 xDiff = 0
 yDiff = 0
-with open("history.json") as data:
-    d = json.load(data)
-    jData = d["locations"]
-    for location in jData:
-        xCor.append(location.get("latitudeE7") / 1E7)
-        yCor.append(location.get("longitudeE7") / 1E7)
-        times.append(location.get("timestampMs"))
-    df = pd.DataFrame({'Longitude': xCor, 'Latitude': yCor, 'Time': times})
-    print(df)
+fileName = "history.json"
 
-for i in range(1,len(xCor)):
-    curXCor = radians(xCor[i])
-    prevXCor = radians(xCor[i-1])
-    curYCor = radians(yCor[i])
-    prevYCor = radians(yCor[i-1])
-    yDiff = abs(curXCor-prevXCor)
-    xDiff = abs(curYCor - prevYCor)
-    curTime = int(times[i])
-    prevTime = int(times[i-1])
-    # print("cur:",curTime)
-    # print("prev",prevTime)
-    tDiff = abs(curTime-prevTime)/60.0/60.0/1000
-    # print("time:", tDiff)
-    a = (sin(yDiff / 2))**2 + cos(prevYCor)*cos(curYCor)*(sin(xDiff/2))**2
-    if ((1-a) > 0 and 1-a < 1):
-        c = 2*atan2(a**0.5, (1 - a)**0.5)
-        dist = 3961 * c
-        # print("Dist:",dist)
-        speed = dist/tDiff
-        # print("Speed:", speed)
-        if (speed < 5):
-            walkDist += dist
-        elif(speed <15):
-            bikeDist += dist
-        else:
-            driveDist += dist
+def setFileName(x):
+    fileName = x
 
-print("walk:",walkDist)
-print("bike:", bikeDist)
-print("drive:",driveDist)
+def getData():
+    with open(fileName) as data:
+        d = json.load(data)
+        jData = d["locations"]
+        for location in jData:
+            xCor.append(location.get("latitudeE7") / 1E7)
+            yCor.append(location.get("longitudeE7") / 1E7)
+            times.append(location.get("timestampMs"))
+        df = pd.DataFrame({'Longitude': xCor, 'Latitude': yCor, 'Time': times})
+        print(df)
+
+    for i in range(1,len(xCor)):
+        curXCor = radians(xCor[i])
+        prevXCor = radians(xCor[i-1])
+        curYCor = radians(yCor[i])
+        prevYCor = radians(yCor[i-1])
+        yDiff = abs(curXCor-prevXCor)
+        xDiff = abs(curYCor - prevYCor)
+        curTime = int(times[i])
+        prevTime = int(times[i-1])
+        tDiff = abs(curTime-prevTime)/60.0/60.0/1000
+        a = (sin(yDiff / 2))**2 + cos(prevYCor)*cos(curYCor)*(sin(xDiff/2))**2
+        if ((1-a) > 0 and 1-a < 1):
+            c = 2*atan2(a**0.5, (1 - a)**0.5)
+            dist = 3961 * c
+            speed = dist/tDiff
+            if (speed < 5):
+                walkDist += dist
+            elif(speed <15):
+                bikeDist += dist
+            else:
+                driveDist += dist
+
+def getTotalDistance():
+    return walkDist+bikeDist+driveDist
+
+def getWalkDistance():
+    return walkDist
+
+def getDriveDistance():
+    return driveDist
+
+def getBikeDistance():
+    return bikeDist
+
+getData()
